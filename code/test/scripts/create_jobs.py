fbf05bd15bfcd6ec22586de8090d8dee06de2230
bisca <francesco.biscacciacarrara@studenti.unipd.it>
2025-03-21 12:21:55 +0100

    v1.0.1

---
+++ code/test/scripts/create_jobs.py
@@ -0,0 +1,229 @@
+import sys
+
+sys.dont_write_bytecode = True
+
+import os, shutil, pandas as pd
+from pathlib import Path
+
+def clear_dir(dir: Path):
+    for item in dir.iterdir():
+        if item.is_file() or item.is_symlink():
+            item.unlink()
+        elif item.is_dir():
+            shutil.rmtree(item)
+
+def main():
+    input_csv = "fHard_instances.csv"
+    if (
+            input(
+                f"Insert y if the file is correct: "
+            )
+            != "y"
+    ): exit(1)
+
+    df = pd.read_csv(input_csv)
+
+    instances = df["Instance"];
+
+
+    code_dir = os.path("../..")
+
+    # jobs
+    jobs_folder = os.path.join(code_dir, "test/jobs")
+    jobs_outputs = os.path.join(code_dir, "test/jobs_output")
+
+    instances_folder = os.path.join(code_dir, "data") #TO BE CHANGED ON CLUSTER
+    exec_dir = os.path.join(code_dir, "build")
+
+    print(f"Jobs folder: {jobs_folder}")
+    print(f"Jobs output: {jobs_outputs}")
+    print(f"Instances folder: {instances_folder}")
+    print(f"ACS Executable: {exec_dir}/CPLEXRun")
+    print(f"CPLEX Executable: {exec_dir}/CPLEXRun")
+
+    if input("Insert y if it's all correct: ") != "y":
+            exit(1)
+
+    os.makedirs(jobs_folder, exist_ok=True)
+    os.makedirs(jobs_outputs, exist_ok=True)
+    clear_dir(Path(jobs_outputs))
+
+    for instance in instances:
+        for exe in ("CPLEXRun","ACS"):
+            job = f"{jobs_folder}/{instance}_CPLEXRun"
+            job_content = f"""#!/bin/bash
+    #SBATCH --job-name={instance}_CPLEXRun
+    #SBATCH --partition=arrow
+    #SBATCH --ntasks=1
+    #SBATCH --mem=32GB
+    #SBATCH --time=00:10:00
+    #SBATCH --output={jobs_outputs}/{instance}_CPLEXRun.out
+    #SBATCH --error={jobs_outputs}/{instance}_CPLEXRun_.out
+
+    # warm up processors
+    sudo cpupower frequency-set -g performance
+    sleep 0.1
+    stress-ng -c 4 --cpu-ops=100
+    # set limits
+    ulimit -v 16777216
+
+    #####################
+
+    {exec_dir}/./{exe} -f {instances_folder}/{instance}.mps -tl 300 
+
+    #####################
+
+    # back to power saving mode
+    sudo cpupower frequency-set -g powersave"""
+
+        with open(job, "w") as f:
+            f.write(job_content)
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+
+def clear_dir(dir: Path):
+    for item in dir.iterdir():
+        if item.is_file() or item.is_symlink():
+            item.unlink()
+        elif item.is_dir():
+            shutil.rmtree(item)
+
+
+def main():
+    # verify input correctness
+    if len(sys.argv) <= 1 or sys.argv[1] in ["-h", "--h", "-help", "--help"]:
+        print(
+            f"Usage: >> python3 {os.path.basename(__file__)} <instances_folder> <all parameters to pass to the execution>."
+        )
+        exit(0)
+
+    # read inputs
+    instances_folder = os.path.abspath(sys.argv[1])
+    execution_parameters = " ".join(sys.argv[2:])
+
+    # verify inputs
+    if (
+        input(
+            f"The instances will be taken from the folder {instances_folder}\nInsert y if it's correct: "
+        )
+        != "y"
+    ):
+        exit(0)
+    if (
+        input(
+            f"The execution will have the following structure:\n>> ./exec -f <instance> <pars>\nInsert y if it's correct: "
+        )
+        != "y"
+    ):
+        exit(0)
+
+    instances_list = os.listdir(instances_folder)
+
+    code_dir = os.path.abspath("../..")
+
+    # jobs
+    jobs_folder = os.path.join(code_dir, "test/jobs")
+    jobs_outputs = os.path.join(code_dir, "test/jobs_output")
+
+    exec_dir = os.path.join(code_dir, "build")
+
+    print(f"Jobs folder: {jobs_folder}")
+    print(f"Jobs output: {jobs_outputs}")
+    print(f"Executable: {exec_dir}/CPLEXRun")
+    if input("Check those paths.\nInsert y if it's all correct: ") != "y":
+        exit(0)
+
+    os.makedirs(jobs_folder, exist_ok=True)
+    os.makedirs(jobs_outputs, exist_ok=True)
+    clear_dir(Path(jobs_outputs))
+
+    for inst in instances_list:
+
+        job = f"{instances_folder}/{inst.replace('.mps', '_job.sh')}"
+        job_content = f"""#!/bin/bash
+#SBATCH --job-name={inst}_CPLEXRun
+#SBATCH --partition=arrow
+#SBATCH --ntasks=1
+#SBATCH --mem=20GB
+#SBATCH --time=00:10:00
+#SBATCH --output={jobs_outputs}/{inst}.out
+#SBATCH --error={jobs_outputs}/{inst}.out
+
+# warm up processors
+sudo cpupower frequency-set -g performance
+sleep 0.1
+stress-ng -c 4 --cpu-ops=100
+# set limits
+ulimit -v 16777216
+
+#####################
+
+{exec_dir}/./CPLEXRun -f {instances_folder}/{inst} -t 300 
+
+#####################
+
+# back to power saving mode
+sudo cpupower frequency-set -g powersave"""
+
+        with open(job, "w") as f:
+            f.write(job_content)
+
+
+if __name__ == "__main__":
+    main()
+    '''
